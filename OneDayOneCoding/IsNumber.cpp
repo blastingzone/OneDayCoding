53e77004af484b9b5c31948608a8d997b40e7d3d
1기 문진상 <blastingzone@naver.com>
2014-04-20 21:03:01 +0900

    업글됨

--- OneDayOneCoding/IsNumber.cpp
+++ OneDayOneCoding/IsNumber.cpp
@@ -3,24 +3,37 @@
 
 bool isNumber(const char* c)
 {
-	// re1 == number?
-	std::regex re1( "[' ']*[0-9]+[' ']*" );
+	// re1 == (+-)number?
+	std::regex re1( "[' ']*[+-]?[0-9]+[' ']*" );
 	// == regex re1( "[' ']*[0-9]+[' ']*" );
 
-	// re2 == number( e )number?
-	std::regex re2( "[' ']*[0-9]+e[0-9]+[' ']*" );
+	// re2 == (+-)number(.)( e )(+-)number?
+	std::regex re2( "[' ']*[+-]?[0-9]+[.]?e[+-]?[0-9]+[' ']*" );
 
-	// re3 == number.number?
-	std::regex re3( "[' ']*[0-9]+[.][0-9]+[' ']*" );
+	// re3 ==  (+-)number.(number)?
+	std::regex re3( "[' ']*[+-]?[0-9]+[.][0-9]*[' ']*" );
 
-	return ( std::regex_match(c, re1) || std::regex_match(c, re2) || std::regex_match(c, re3));
+	// re4 ==  (+-)(number).number?
+	std::regex re4( "[' ']*[+-]?[0-9]*[.][0-9]+[' ']*" );
+	
+	bool test1 = std::regex_match( c, re1 );
+	bool test2 = std::regex_match( c, re2 );
+	bool test3 = std::regex_match( c, re3 );
+	bool test4 = std::regex_match( c, re4 );
+
+	return ( std::regex_match( c, re1 ) || std::regex_match( c, re2 ) || std::regex_match( c, re3 ) || std::regex_match( c, re4 ) );
 }
 
+
+void PrintTestSet( const char* test )
+{
+	printf( "test : %s : \"%s\"\n", isNumber( test ) ? "true" : "false", test );
+}
 
 void main()
 {
 	const char* test1 = "12345";
-	const char* test2 = " 12345 ";
+	const char* test2 = "     12345 ";
 	const char* test3 = " 123e1234 ";
 	const char* test4 = "abcd";
 	const char* test5 = "1.0";
@@ -28,16 +41,38 @@ void main()
 	const char* test7 = "1 a";
 	const char* test8 = "2e10";
 	const char* test9 = "2eee10";
+	const char* test10 = "2e"; // false
+	const char* test11 = "3.e5"; // true
+	const char* test12 = "+3."; // true
+	const char* test13 = "+"; // false
+	const char* test14 = "+++3"; // false
+	const char* test15 = "-+3."; // false
+	const char* test16 = "-3.e-5"; // true
+	const char* test17 = "3.e+5"; // true
+	const char* test18 = "+3.e5"; // true
+	const char* test19 = "  "; // false
 
-	printf( "test 1 : %s : \"%s\"\n", isNumber( test1 ) ? "true" : "false" , test1 );
-	printf( "test 2 : %s : \"%s\"\n", isNumber( test2 ) ? "true" : "false" , test2 );
-	printf( "test 3 : %s : \"%s\"\n", isNumber( test3 ) ? "true" : "false", test3 );
-	printf( "test 4 : %s : \"%s\"\n", isNumber( test4 ) ? "true" : "false", test4 );
-	printf( "test 5 : %s : \"%s\"\n", isNumber( test5 ) ? "true" : "false", test5 );
-	printf( "test 6 : %s : \"%s\"\n", isNumber( test6 ) ? "true" : "false", test6 );
-	printf( "test 7 : %s : \"%s\"\n", isNumber( test7 ) ? "true" : "false", test7 );
-	printf( "test 8 : %s : \"%s\"\n", isNumber( test8 ) ? "true" : "false", test8 );
-	printf( "test 9 : %s : \"%s\"\n", isNumber( test9 ) ? "true" : "false", test9 );
+
+	PrintTestSet( test1 );
+	PrintTestSet( test2 );
+	PrintTestSet( test3 );
+	PrintTestSet( test4 );
+	PrintTestSet( test5 );
+	PrintTestSet( test6 );
+	PrintTestSet( test7 );
+	PrintTestSet( test8 );
+	PrintTestSet( test9 );
+	PrintTestSet( test10 );
+	PrintTestSet( test11 );
+	PrintTestSet( test12 );
+	PrintTestSet( test13 );
+	PrintTestSet( test14 );
+	PrintTestSet( test15 );
+	PrintTestSet( test16 );
+	PrintTestSet( test17 );
+	PrintTestSet( test18 );
+	PrintTestSet( test19 );
+
 
 	getchar();
 
